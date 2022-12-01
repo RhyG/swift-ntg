2fab052a551bc610017af9ecc10a38f3c1652b42
rhyg <rhysgeary@gmail.com>
2022-12-01 19:55:28 +1000

    Initial Commit

---
+++ NTG/NTGApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTGApp.swift
+//  NTG
+//
+//  Created by Rhys Geary on 1/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct NTGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
